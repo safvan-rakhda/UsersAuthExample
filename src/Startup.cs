bb7fc5ec088779ab08003488011a87f1efaf5f57
safvan-rakhda <smrakhda@gmail.com>
2022-10-13 10:36:14 +0530

    Use swagger

--- src/Startup.cs
+++ src/Startup.cs
@@ -95,8 +95,8 @@ namespace UsersAuthExample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UsersAuthExample v1"));
@@ -118,7 +118,7 @@ namespace UsersAuthExample
                 //        options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
                 //    }
                 //});
-            }
+            //}
 
             app.UseHttpsRedirection();
 
